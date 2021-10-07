de2bb104805a249356e4c760da5cfad48768a29c
Gabriel Smith <gabrielsmith@Gabriels-MacBook-Pro-2.local>
2021-10-07 17:09:28 -0500

    basic joke functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,9 @@
 
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
-import jokeData from './helpers/jokeData';
+import domEvents from './events/domEvents';
 
 const init = () => {
-  document.querySelector('#app').innerHTML = `
-  `;
-
-  jokeData().then(console.warn);
+  domEvents();
 };
-
 init();
